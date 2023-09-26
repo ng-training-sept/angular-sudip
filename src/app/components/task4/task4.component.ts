17da4870b2a51c6f72c19e6020618e3193b6ab46
Manandhar <sudip.manandhar@cotiviti.com>
2023-09-26 08:06:17 +0545

    task 4 done...

--- src/app/components/task4/task4.component.ts
+++ src/app/components/task4/task4.component.ts
@@ -1,14 +1,18 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-task4',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule],
   templateUrl: './task4.component.html',
   styleUrls: ['./task4.component.scss']
 })
 export class Task4Component {
+
+  inputFieldStyle: string = 'height: 10px; padding: 10px; margin: 0px 10px';
+
   // to recieve values from parent
   @Input({required: true}) titles: string[] = [];
 
@@ -17,7 +21,7 @@ export class Task4Component {
   @Output() titleEvent$ = new EventEmitter<string>();
 
   detectInput(title: any): void {
-    this.titleEvent$.emit(title.target.value);
-    console.log(title.target.value);
+    this.titleEvent$.emit(title);
+    console.log(title);
   }
 }
