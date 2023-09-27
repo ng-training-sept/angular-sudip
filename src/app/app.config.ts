3980eac4969100fa2a83452870163e5168e66a71
Manandhar <sudip.manandhar@cotiviti.com>
2023-09-27 09:32:12 +0545

    task5 done...

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,16 +2,18 @@ import { ApplicationConfig } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { Routes, provideRouter } from '@angular/router';
 import { GroceryComponent } from './forms/grocery/grocery.component';
-import { SportsComponent } from './forms/sports/sports.component';
-import { CardItemComponent } from './components/gcard/card-item/card-item.component';
+// import { SportsComponent } from './forms/sports/sports.component';
+// import { CardItemComponent } from './components/gcard/card-item/card-item.component';
 
 
 export const routes: Routes = [
+  { path: '', loadComponent: () => import('./forms/home/home.component').then(m => m.HomeComponent) },
+  { path: 'home', loadComponent: () => import('./forms/home/home.component').then(m => m.HomeComponent) },
   { path: 'grocery', component: GroceryComponent },
   {path: 'grocery', loadChildren: () => import('./forms/grocery/grocery.routes').then(m => m.routes)},
-  // { path: 'card-item/:id', component: CardItemComponent},
   { path: 'sports', loadComponent: () => import('./forms/sports/sports.component').then(m => m.SportsComponent) },
-  {path: 'sports', loadChildren: () => import('./forms/sports/sports.routes').then(m => m.routes)}  
+  {path: 'sports', loadChildren: () => import('./forms/sports/sports.routes').then(m => m.routes)},
+  { path: '**', loadComponent: () => import('./forms/notfound/notfound.component').then(m => m.NotfoundComponent) },  
 ];
 
 
