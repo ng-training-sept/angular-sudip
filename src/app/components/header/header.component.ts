8c6a1c87e53f952362927ba55409782cba4b56b9
Manandhar <sudip.manandhar@cotiviti.com>
2023-09-28 08:12:14 +0545

    DI ..

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,17 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatMenuModule } from '@angular/material/menu';
 import { RouterLink } from '@angular/router';
+import { AuthService } from 'src/app/service/auth/auth.service';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, MatToolbarModule, MatMenuModule, MatButtonModule, RouterLink],
+  imports: [CommonModule, MatToolbarModule, MatMenuModule, MatButtonModule, RouterLink, MatIconModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  private readonly authService = inject(AuthService);
 
+
+  
+  login(): void {
+    console.log("Test", this.authService.isAuthenticated());
+    this.authService.authenticate();
+    console.log("Test", this.authService.isAuthenticated());
+  }
 }
