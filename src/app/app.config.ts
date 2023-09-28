ae61275a9d73ad45bc5c1cdc342b536bb24683a5
Manandhar <sudip.manandhar@cotiviti.com>
2023-09-28 07:40:59 +0545

    minor changes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,18 +5,42 @@ import { GroceryComponent } from './forms/grocery/grocery.component';
 // import { SportsComponent } from './forms/sports/sports.component';
 // import { CardItemComponent } from './components/gcard/card-item/card-item.component';
 
-
 export const routes: Routes = [
-  { path: '', loadComponent: () => import('./forms/home/home.component').then(m => m.HomeComponent) },
-  { path: 'home', loadComponent: () => import('./forms/home/home.component').then(m => m.HomeComponent) },
+  {
+    path: '',
+    loadComponent: () =>
+      import('./forms/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./forms/home/home.component').then((m) => m.HomeComponent),
+  },
   { path: 'grocery', component: GroceryComponent },
-  {path: 'grocery', loadChildren: () => import('./forms/grocery/grocery.routes').then(m => m.routes)},
-  { path: 'sports', loadComponent: () => import('./forms/sports/sports.component').then(m => m.SportsComponent) },
-  {path: 'sports', loadChildren: () => import('./forms/sports/sports.routes').then(m => m.routes)},
-  { path: '**', loadComponent: () => import('./forms/notfound/notfound.component').then(m => m.NotfoundComponent) },  
+  {
+    path: 'grocery',
+    loadChildren: () =>
+      import('./forms/grocery/grocery.routes').then((m) => m.routes),
+  },
+  {
+    path: 'sports',
+    loadComponent: () =>
+      import('./forms/sports/sports.component').then((m) => m.SportsComponent),
+  },
+  {
+    path: 'sports',
+    loadChildren: () =>
+      import('./forms/sports/sports.routes').then((m) => m.routes),
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./forms/notfound/notfound.component').then(
+        (m) => m.NotfoundComponent
+      ),
+  },
 ];
 
-
 export const appConfig: ApplicationConfig = {
-  providers: [provideAnimations(), provideRouter(routes)]
+  providers: [provideAnimations(), provideRouter(routes)],
 };
